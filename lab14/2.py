e9a49f6e4683c3b96538bb1c75650cbe8eab97a7
Suddeness <lezer194@gmail.com>
2025-03-05 14:52:58 +0100

    Update 2.py

--- lab14/2.py
+++ lab14/2.py
@@ -4,6 +4,25 @@ import matplotlib.pyplot as plt
 x=[]
 y=[]
 z=[]
+def grfk(name,n=0):
+    plt.title('Population, ages 7-11, total', fontsize=15)
+    plt.xlabel('Year', fontsize=12, color='red')
+    plt.ylabel('Indicator', fontsize=12, color='red')
+    plt.grid(True)
+    if n == 0:#2.1
+        plt.plot(x, y, label="First_Country", color="purple")
+        plt.plot(x, z, label="Second_Country", color="yellow")
+    else:#2.2
+            usr_inp = (input("enter country (first/second): ")).lower()
+            if usr_inp == "first":
+                plt.bar(x,y)
+            elif usr_inp == "second":
+                plt.bar(x, z)
+            else:
+                print("wrong value")
+    plt.savefig(name, dpi=300)
+    plt.close()
+
 with open("data_file.csv", "r") as file:
     reader = csv.reader(file)
     next(reader)
@@ -12,18 +31,5 @@ with open("data_file.csv", "r") as file:
             x.append(int(line[0]))
             y.append(int(line[1]))
             z.append(int(line[2]))
-plt.plot(x,z,label="First_Country", color="purple")
-plt.plot(x,y,label="Second_Country", color="yellow")
-plt.title('Population, ages 7-11, total', fontsize=15)
-plt.xlabel('Year', fontsize=12, color='red')
-plt.ylabel('Indicator', fontsize=12, color='red')
-plt.legend()
-plt.grid(True)
-plt.savefig("plot2.png",dpi=300)
-
-
-fig, ax = plt.subplots()
-ax.pie(y, labels = x)
-ax.axis("equal")
-plt.legend()
-plt.savefig("plot3.png",dpi=300)
+    grfk("plot2.png")
+    grfk("plot3.png",1)
